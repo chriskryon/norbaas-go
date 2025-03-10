181bcc6f98967df1b490571016e1de16c7877034
Christopher <contato.christopher@gmail.com>
2025-03-09 21:31:04 -0300

    add get started section and hello world example

---
+++ 03_get_started/get_started.go
@@ -0,0 +1,36 @@
+/*
+
+Go Get Started:
+Para começar a utilizar o Go, precisamos de duas coisas:
+- Um editor de texto ou IDE para escrever o código, como o Visual Studio Code, Atom, Sublime Text, etc.
+- Compilador como o GCC, para traduzir o código Go em código de máquina.
+
+Instalação:
+Para instalar o Go, acesse o site oficial: https://golang.org/dl/
+Siga as instruções de instalação para o seu sistema operacional.
+
+Após a instalação, abra o terminal e digite o comando `go version` para verificar se a instalação foi bem-sucedida.
+
+Instalação de uma IDE:
+Sugiro a instalação do Visual Studio Code, pois é uma IDE leve e fácil de usar.
+Para instalar o Visual Studio Code, acesse o site oficial: https://code.visualstudio.com/
+Siga as instruções de instalação para o seu sistema operacional.
+
+Abra o editor VS Code
+Abrir o gerenciador de extensões ou, alternativamente, pressionar Ctrl + Shift + x
+Na caixa de pesquisa, digitar "go" e pressionar Enter
+Encontrar a extensão Go da equipe GO do Google e instale a extensão
+Após a instalação, abra a paleta de comandos pressionando Ctrl + Shift + p
+Executar o comando Go: Install/Update Tools
+Selecionar todas as ferramentas fornecidas e clicar em OK
+
+Agora, você está pronto para começar a escrever código Go.
+Abra o terminal e digite go mod init example.com/hello para criar um módulo Go.
+
+Um módulo Go é uma coleção de pacotes Go que são versionados juntos como uma unidade. Ele é definido por um arquivo go.mod, que especifica o nome do módulo e as dependências necessárias.
+
+O que faz go mod init?
+O comando go mod init cria um novo arquivo go.mod no diretório atual. Este arquivo contém informações sobre o módulo, incluindo seu nome e as dependências que ele usa.
+Seria como se fosse um npm init.
+
+*/
---
+++ 03_get_started/hello_world.go
@@ -0,0 +1,9 @@
+package main // Este é o pacote principal do nosso programa
+
+import (
+	"fmt" // Estamos importando o pacote fmt, que nos permite formatar e imprimir texto
+)
+
+func main() { // A função main é o ponto de entrada do nosso programa
+	fmt.Println("Hello World!") // Aqui estamos imprimindo "Hello World!" na tela
+}
