3a076a31bd3cd584a38330211390f9834c219d6d
Christopher <contato.christopher@gmail.com>
2025-03-09 21:12:26 -0300

    add tutorial section

---
+++ 02_tutorial/tutorial.go
@@ -0,0 +1,57 @@
+/*
+
+O que é o Go?
+- É uma linguagem de programação open-source e multiplataforma
+- Conhecido por ser uma linguagem de programação de alto desempenho
+- É uma linguagem rápida, estaticamente tipada e compilada, simples e eficiente
+- A sintaxe é semelhante ao C++, mas com recursos adicionais
+- Foi criado pelo Google em 2007 e lançado em 2009
+
+
+Para que serve o Go?
+- Desenvolver:
+	- Aplicações server-side 
+	- Programas baseados em rede
+	- Aplicações empresariais multiplataforma
+	- Nuvem e serviços de armazenamento
+
+Por que usar o Go?
+- Divertido e fácil de aprender
+- Tempo de compilação rápido, assim como a execução
+- Suporta simultaneidade
+- Tem um sistema de gerenciamento de memória eficiente
+- Multiplataforma
+- Concorrencia: Concorrência é executar várias coisas fora de ordem, ou ao mesmo tempo, sem afetar o resultado final
+
+Resumo de Go, Python e C++:
+- Go:
+	- Estaticamente tipado
+	- Tempo de execução rápido
+	- Compilado
+	- Tempo de compilação rápido
+	- Suporta simultaneidade por meio de goroutines e canais
+	- Possui coleta automática de lixo
+	- Não suporta classes e objetos
+	- Não suporta herança
+
+- Python:
+	- Digitado dinamicamente
+	- Tempo de execução lento
+	- Interpretado
+	- Interpretado
+	- Nenhum mecanismo de simultaneidade integrado
+	- Possui coleta automática de lixo
+	- Tem classes e objetos
+	- Suporta herança
+
+- C++:
+	- Estaticamente tipado
+	- Tempo de execução rápido
+	- Compilado
+	- Tempo de compilação lento
+	- Suporta simultaneidade por meio de threads
+	- Não possui coleta automática de lixo
+	- Tem classes e objetos
+	- Suporta herança
+
+*/
